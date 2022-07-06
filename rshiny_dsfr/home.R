8f248e6538f3b806687a1f41f820469a8513fa16
virgilecs <108006208+virgilecs@users.noreply.github.com>
2022-07-06 10:12:53 +0200

    Delete rshiny_dsfr directory

--- rshiny_dsfr/home.R
+++
@@ -1,2 +0,0 @@
-  source("rshiny_dsfr/user/ui.R",encoding = "UTF-8",local=T)
-  source("rshiny_dsfr/user/server.R",encoding = "UTF-8",local=T)
--- rshiny_dsfr/user/ui.R
+++
@@ -1,7 +0,0 @@
-output$pageStub <- renderUI(fluidPage(tags$div(class='fr-container fr-py-16v',
-                                               # Mettre vos graphiques ci-dessous
-                                                        numericInput('n', 'Number of obs', 100),
-                                                        plotOutput(plot)
-)))
-
-
