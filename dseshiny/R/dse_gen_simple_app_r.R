aabf2b8e95fa4d7e0b01515a05df020deaff910a
V‏irgile Cohen-Solal <virgile.cohen-solal@i-carre.net>
2022-07-19 09:40:03 +0200

    commit fonction qui fonctionne bien

--- dseshiny/R/dse_gen_simple_app_r.R
+++ dseshiny/R/dse_gen_simple_app_r.R
@@ -8,16 +8,14 @@
 #' @export
 
 library(stringr)
-fichier <- "C:/Travail/package3/dseshiny/dseshiny/shiny_default_app/shiny_default_app.zip" #chemin du fichier zip shiny_default_app.zip à définir manuellement par l'utilisateur
 
 dse_gen_simple_app_r <- function(chemin){
   if (dir.exists(chemin)) {
     print("Le chemin fournit en parametre de la fonction existe deja, veuillez fournir un chemin qui n existe pas deja")
   } else {
     dir.create(chemin)
-    destination <- str_c(chemin, "/shiny_default_app.zip")
-    file.copy(fichier, destination)
-    unzip(destination, exdir = chemin)
+    download.file("https://github.com/virgilecs/shiny_app/archive/refs/heads/main.zip", destfile = "shiny_app_main.zip")
+    unzip("shiny_app_main.zip", exdir = chemin)
 
 
   }
